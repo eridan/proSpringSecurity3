71f32059c7fcb2e26a420f732005f09d2955c3ba
eridan <null>
2012-08-29 11:24:23 +0100

    Adding Custom Login Page

---
+++ src/main/java/com/ibm/bim/spring/security/prospring3/web/BaseController.java
@@ -0,0 +1,10 @@
+package com.ibm.bim.spring.security.prospring3.web;
+
+
+/**
+ * The base Spring MVC controller. Used to provide common functionality to all controllers.
+ * 
+ * @author Mularien
+ */
+public class BaseController {
+}
