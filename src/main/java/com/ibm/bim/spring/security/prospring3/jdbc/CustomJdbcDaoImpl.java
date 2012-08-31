99c29a0696eba82993470298853c71dd52c8bccb
eridan <null>
2012-08-31 12:09:46 +0100

    PW encoded for Change PW functionality

--- src/main/java/com/ibm/bim/spring/security/prospring3/jdbc/CustomJdbcDaoImpl.java
+++ src/main/java/com/ibm/bim/spring/security/prospring3/jdbc/CustomJdbcDaoImpl.java
@@ -5,14 +5,26 @@
 package com.ibm.bim.spring.security.prospring3.jdbc;
 
 import com.ibm.bim.spring.security.prospring3.security.IChangePassword;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.dao.SaltSource;
+import org.springframework.security.authentication.encoding.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 
 public class CustomJdbcDaoImpl extends JdbcDaoImpl implements IChangePassword {
+    
+    @Autowired
+    private SaltSource saltSource;
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public void changePassword(String userName, String password) {
-        System.out.println("Change the pw method");
+        UserDetails user = loadUserByUsername(userName);
+        String encodedPw = passwordEncoder.encodePassword(password,saltSource.getSalt(user));
         getJdbcTemplate().update(
-                "UPDATE USERS SET PASSWORD = ? WHERE USERNAME = ?", password, userName);
+                "UPDATE USERS SET PASSWORD = ? WHERE USERNAME = ?", encodedPw, userName);
+        System.out.println("Password was chenged for the user ["+userName+"] "+encodedPw);
     }
 }
